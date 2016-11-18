1c5cac4f2f397ebce7c970545e43c15059e793d0
wilmer de la rosa <wilmer.delarosa.santana@gmail.com>
2016-11-18 00:29:57 -0430

    Revert "Revert "commit""
    This reverts commit a1f41eb9859b21e91e9ff297ef01bd987e7feace.

---
+++ proyecto/prueba/modulo_opciones/configuracion_modulos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using puntoVenta;
+using puntoVenta.clases;
+
+namespace puntoVenta.modulo_opciones
+{
+
+    public partial class configuracion_modulos : FormBase
+    {
+
+
+        //objetos
+        Utilidades utilidades=new Utilidades();
+
+
+        //litas
+
+
+
+
+        public configuracion_modulos()
+        {
+            InitializeComponent();
+            loadVentana();
+        }
+
+        public override void loadVentana()
+        {
+            this.Text = utilidades.GetTituloVentana("wilmer de la rosa", "configuración módulos");
+            this.tituloLabel.Text = utilidades.GetTituloVentana("wilmer de la rosa", "configuración módulos");
+
+        }
+
+        private void configuracion_modulos_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
