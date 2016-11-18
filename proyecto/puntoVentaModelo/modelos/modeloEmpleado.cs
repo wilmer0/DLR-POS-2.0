cb4d01fb6a5a81fa51e1cb8010fe7200eebf80c4
wilmer de la rosa <wilmer.delarosa.santana@gmail.com>
2016-11-18 00:48:09 -0430

    commit

---
+++ proyecto/puntoVentaModelo/modelos/modeloEmpleado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Windows.Forms;
+namespace puntoVentaModelo.modelos
+{
+    public class modeloEmpleado
+    {
+
+
+        public Boolean validarLogin()
+        {
+            try
+            {
+
+
+                return true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error validarLogin.: "+ex.ToString());
+                return false;
+            }
+        }
+    }
+}
