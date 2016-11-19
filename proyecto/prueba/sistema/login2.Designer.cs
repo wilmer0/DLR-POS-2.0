29cf84fa8a70fe8f67027b8bc6416159d6878218
wilmer de la rosa <wilmer.delarosa.santana@gmail.com>
2016-11-19 13:14:05 -0430

    commit

---
+++ proyecto/prueba/sistema/login2.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace puntoVenta.sistema
+{
+    partial class login2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(369, 5);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 274);
+            this.panel1.Size = new System.Drawing.Size(510, 54);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(186, 5);
+            // 
+            // panel2
+            // 
+            this.panel2.Size = new System.Drawing.Size(762, 37);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(35, 78);
+            this.label1.Size = new System.Drawing.Size(71, 20);
+            this.label1.Text = "Usuario";
+            // 
+            // login2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 340);
+            this.Name = "login2";
+            this.Text = "login2";
+            this.Load += new System.EventHandler(this.login2_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ proyecto/prueba/sistema/login2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puntoVenta.sistema
+{
+    public partial class login2 : FormBase
+    {
+        public login2()
+        {
+            InitializeComponent();
+        }
+
+        private void login2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
