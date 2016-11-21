fd922ae8a9cff311651de71195f4ea74b6461f63
wilmer de la rosa <wilmer.delarosa.santana@gmail.com>
2016-11-20 20:33:03 -0430

    commit

--- proyecto/puntoVentaWin/sistema/menu1.cs
+++ proyecto/puntoVentaWin/sistema/menu1.cs
@@ -7,11 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using puntoVentaModelo;
+using puntoVentaModelo.modelos;
 
 namespace puntoVenta.sistema
 {
     public partial class menu1 : FormBase
     {
+
+
+        //objetos
+        private empleado empleado;
+
+
+
+        //modelos
+        modeloEmpleado modeloEmpleado=new modeloEmpleado();
+
+
+        //listas
+        List<sistema_modulo> listaModulos=new List<sistema_modulo>();
+        List<sistema_modulo_ventanas> listaModuloVentanas=new List<sistema_modulo_ventanas>(); 
+
         public menu1(puntoVentaModelo.empleado empleado)
         {
             InitializeComponent();
