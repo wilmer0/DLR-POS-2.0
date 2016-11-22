b09abaa712943992aae4fb5ae6bc30f431002f25
wilmer de la rosa <wilmer.delarosa.santana@gmail.com>
2016-11-22 01:06:23 -0430

    commit

---
+++ proyecto/puntoVentaWin/modulo_empresa/ventana_empresa.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace puntoVentaWin.modulo_empresa
+{
+    partial class ventana_empresa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ventana_empresa));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.empresaText = new System.Windows.Forms.TextBox();
+            this.RncText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.divisionText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.activoCheck = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(375, 5);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 358);
+            this.panel1.Size = new System.Drawing.Size(516, 54);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(189, 5);
+            // 
+            // panel2
+            // 
+            this.panel2.Size = new System.Drawing.Size(768, 37);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(508, 80);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(270, 26);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 34);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Empresa";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.SkyBlue;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(127, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nombre";
+            // 
+            // empresaText
+            // 
+            this.empresaText.BackColor = System.Drawing.Color.White;
+            this.empresaText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.empresaText.Location = new System.Drawing.Point(139, 137);
+            this.empresaText.MaxLength = 200;
+            this.empresaText.Name = "empresaText";
+            this.empresaText.Size = new System.Drawing.Size(256, 26);
+            this.empresaText.TabIndex = 4;
+            // 
+            // RncText
+            // 
+            this.RncText.BackColor = System.Drawing.Color.White;
+            this.RncText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RncText.Location = new System.Drawing.Point(139, 185);
+            this.RncText.MaxLength = 19;
+            this.RncText.Name = "RncText";
+            this.RncText.Size = new System.Drawing.Size(256, 26);
+            this.RncText.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(92, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Rnc";
+            // 
+            // divisionText
+            // 
+            this.divisionText.BackColor = System.Drawing.Color.White;
+            this.divisionText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divisionText.Location = new System.Drawing.Point(139, 236);
+            this.divisionText.MaxLength = 2;
+            this.divisionText.Name = "divisionText";
+            this.divisionText.Size = new System.Drawing.Size(256, 26);
+            this.divisionText.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(62, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "División";
+            // 
+            // activoCheck
+            // 
+            this.activoCheck.AutoSize = true;
+            this.activoCheck.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.activoCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.activoCheck.Location = new System.Drawing.Point(139, 291);
+            this.activoCheck.Name = "activoCheck";
+            this.activoCheck.Size = new System.Drawing.Size(68, 21);
+            this.activoCheck.TabIndex = 13;
+            this.activoCheck.Text = "Activo";
+            this.activoCheck.UseVisualStyleBackColor = true;
+            // 
+            // empresa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 424);
+            this.Controls.Add(this.activoCheck);
+            this.Controls.Add(this.divisionText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.RncText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.empresaText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "empresa";
+            this.Text = "empresa";
+            this.Load += new System.EventHandler(this.empresa_Load);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.panel2, 0);
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.empresaText, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.RncText, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.divisionText, 0);
+            this.Controls.SetChildIndex(this.activoCheck, 0);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox empresaText;
+        private System.Windows.Forms.TextBox RncText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox divisionText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox activoCheck;
+    }
+}
